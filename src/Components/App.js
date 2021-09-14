74676b722f038afdd425a022f22773631b6c1c12
Tong Allan Zhao <allanzhao@tongs-air.attlocal.net>
2021-09-14 14:52:49 -0700

    Use youtube api to retrieve videos based on search term

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,26 @@
+import axios from 'axios';
 import React from 'react';
+import SearchBar from './SearchBar';
+import youtube from '../apis/youtube';
 
 class App extends React.Component {
+  state = {videoList: []};
+
+  onSearchSubmit = async (term) => {
+    const response = await youtube.get('/search', {
+      params: {
+        q: term
+      }
+    });
+
+    this.setState({videoList: response.data.items});
+  };
   render () {
     return (
-      <div>App</div>
+      <div className="ui container">
+        <SearchBar onSearchSubmit={this.onSearchSubmit}/>
+        I have {this.state.videoList.length} videos
+      </div>
     );
   }
 }
