cb1f8f1271af34fcf046432bb89bcce426828a68
Tong Allan Zhao <allanzhao@tongs-air.attlocal.net>
2021-09-14 16:17:52 -0700

    Display list of youtube videos with a video player on selected video

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,14 @@
-import axios from 'axios';
 import React from 'react';
 import SearchBar from './SearchBar';
+import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 import youtube from '../apis/youtube';
 
 class App extends React.Component {
-  state = {videoList: []};
+  state = {
+    videoList: [],
+    selectedVideo: null
+  };
 
   onSearchSubmit = async (term) => {
     const response = await youtube.get('/search', {
@@ -15,11 +19,16 @@ class App extends React.Component {
 
     this.setState({videoList: response.data.items});
   };
+
+  onVideoSelect = (video) => {
+    this.setState({selectedVideo: video});
+  }
   render () {
     return (
       <div className="ui container">
         <SearchBar onSearchSubmit={this.onSearchSubmit}/>
-        I have {this.state.videoList.length} videos
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList videoList={this.state.videoList} onVideoSelect={this.onVideoSelect}/>
       </div>
     );
   }
